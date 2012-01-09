546ab00842f7600891a0cb49a0f4222d7d3c2dce
Chris Truman <chris@mysterydining.com>
2012-01-09 11:27:10 +0000

    Added stashed changes back in....

--- MyClass.cs
+++ MyClass.cs
@@ -1,12 +1,17 @@
 using System;
 
-namspace com.mysterydining.www
+namespace com.mysterydining.www
 {
 	public class Test
 	{
 		public void DoSomething()
 		{
-			throws new NotImplementedException();
+			throw new NotImplementedException();
+		}
+
+		public void SomethingElse(string test)
+		{
+			test = test + test + test;
 		}
 	}
 }
